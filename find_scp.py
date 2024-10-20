0a7232d5d7f915de726e3dc7cc9edea0b38ec3fa
Mario <6395323+mario-huang@users.noreply.github.com>
2024-10-20 17:12:47 +0800

    scp

--- find_scp.py
+++ find_scp.py
@@ -1,19 +1,23 @@
+from pydicom import Dataset
 from find_scu import findScu
 from share import ae_scp
-from pynetdicom.sop_class import PatientRootQueryRetrieveInformationModelFind, StudyRootQueryRetrieveInformationModelFind, PatientStudyOnlyQueryRetrieveInformationModelFind
+from pynetdicom.sop_class import PatientRootQueryRetrieveInformationModelFind, StudyRootQueryRetrieveInformationModelFind
 
 # Add the supported presentation context
 ae_scp.add_supported_context(PatientRootQueryRetrieveInformationModelFind)
 ae_scp.add_supported_context(StudyRootQueryRetrieveInformationModelFind)
-ae_scp.add_supported_context(PatientStudyOnlyQueryRetrieveInformationModelFind)
 
 # Define a callback function to handle C-FIND requests
 def handle_find(event):
-    ds = event.identifier
+    ds: Dataset = event.identifier
     print(f"Received C-FIND request with dataset: {ds}")
-
+    # Query/Retrieve Level
+    query_level = ds.get((0x0008, 0x0052))
+    query_model = PatientRootQueryRetrieveInformationModelFind
+    if query_level.value == "STUDY":
+        query_model = StudyRootQueryRetrieveInformationModelFind
     # Call findScu function to send the request to the upstream server
-    for status, response in findScu(ds):
+    for status, response in findScu(ds, query_model):
         if status.Status in (0xFF00, 0xFF01):  # Pending status
             print(f"Forwarding response: {response}")
             yield status.Status, response
--- move_scu.py
+++ move_scu.py
@@ -1,11 +1,14 @@
 from typing import Iterator, Tuple
+from pynetdicom.sop_class import PatientRootQueryRetrieveInformationModelMove, StudyRootQueryRetrieveInformationModelMove, PatientStudyOnlyQueryRetrieveInformationModelMove
 from pydicom.dataset import Dataset
 from pynetdicom import AE
 
-def findScu(ds: Dataset, query_model: str) -> Iterator[Tuple[Dataset, Dataset | None]]:
+def moveScu(ds: Dataset) -> Iterator[Tuple[Dataset, Dataset | None]]:
     ae_scu = AE()
     # Add the requested presentation context
-    ae_scu.add_requested_context(query_model)
+    ae_scu.add_requested_context(PatientRootQueryRetrieveInformationModelMove)
+    ae_scu.add_requested_context(StudyRootQueryRetrieveInformationModelMove)
+    ae_scu.add_requested_context(PatientStudyOnlyQueryRetrieveInformationModelMove)
     # Connect to the SCP server, port number 4242 (according to actual SCP configuration)
     assoc = ae_scu.associate("192.168.3.100", 4242)
 
@@ -13,7 +16,7 @@ def findScu(ds: Dataset, query_model: str) -> Iterator[Tuple[Dataset, Dataset | 
         print("Connection to upstream server established.")
 
         # Send C-FIND request and receive responses
-        responses = assoc.send_c_find(ds, query_model)
+        responses = assoc.send_c_move(ds, 'STORE_SCP', PatientRootQueryRetrieveInformationModelMove)
 
         # Yield each response from the upstream server
         for status, identifier in responses:
