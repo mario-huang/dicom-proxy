5e64abb5b9fe7e05d38719bed2b07e5bd8cb4914
Mario <6395323+mario-huang@users.noreply.github.com>
2024-10-21 12:16:58 +0800

    store

--- store_scp.py
+++ store_scp.py
@@ -8,17 +8,10 @@ ae_scp.supported_contexts = AllStoragePresentationContexts
 
 def handle_store(event):
     """处理接收到的 C-STORE 请求"""
-    ds = event.dataset
+    ds: Dataset = event.dataset
     print(f"Received and forwarded image: {ds.SOPInstanceUID}")
     # 直接将图像发送给客户端
     # 这里假设 move_destination、move_addr 和 move_port 是可用的
     storeScu(ds, "127.0.0.1", 4242)
-    # for status, identifier in responses:
-    #     if status in (0xFF00, 0xFF01):  # Pending 状态
-    #         # print(f"Forwarding response: {identifier}")
-    #         yield status, identifier
-    #     yield status, identifier
-    # yield identifier
-
-    # 返回成功状态
+    # Return a 'Success' status
     return 0x0000
--- store_scu.py
+++ store_scu.py
@@ -4,21 +4,18 @@ from pynetdicom import AE, StoragePresentationContexts
 def storeScu(ds: Dataset, addr: str, port: int) -> None:
     """将接收到的图像通过 C-STORE 发送给客户端"""
     ae_scu = AE()
-
     # 添加 Storage SCP 支持的 Presentation Context
     ae_scu.requested_contexts = StoragePresentationContexts
 
     # 与目标客户端建立关联
     assoc = ae_scu.associate(addr, port)
     if assoc.is_established:
+        print(f"Connection established with {addr} for C-STORE.")
         # Use the C-STORE service to send the dataset
         # returns the response status as a pydicom Dataset
         status = assoc.send_c_store(ds)
-        print(f"Connection established with {addr} for C-STORE.")
-        
         # Check the status of the storage request
         if status:
-        # If the storage request succeeded this will be 0x0000
             print('C-STORE request status: 0x{0:04x}'.format(status.Status))
         else:
             print('Connection timed out, was aborted or received invalid response')
