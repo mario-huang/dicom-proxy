91766d8b578372734f6d7d68fccb64b3d78a9306
Mario <6395323+mario-huang@users.noreply.github.com>
2024-10-22 22:38:52 +0800

    store

--- store_scp.py
+++ store_scp.py
@@ -1,30 +1,28 @@
-import os
-import threading
-from pydicom import dcmread
-from pydicom.dataset import Dataset
 from pynetdicom import AllStoragePresentationContexts
-from share import ae_scp, store_status_queue
-from store_scu import storeScu
+from share import ae_scp, move_queue
 
 # Add the Storage SCP's supported presentation contexts
 ae_scp.supported_contexts = AllStoragePresentationContexts
 
+
+# Implement a handler for evt.EVT_C_STORE
 def handle_store(event):
-    """处理接收到的 C-STORE 请求"""
-    ds: Dataset = event.dataset
+    # Decode the C-STORE request's *Data Set* parameter to a pydicom Dataset
+    ds = event.dataset
+    # Add the File Meta Information
     ds.file_meta = event.file_meta
     # print(f"Received and forwarded image: {ds.SOPInstanceUID}")
     # Save the dataset using the SOP Instance UID as the filename
     # ds.save_as(ds.SOPInstanceUID, write_like_original=False)
     # 将数据集保存到临时文件
     # ds.save_as(ds.SOPInstanceUID, write_like_original=False)
-    
+
     # 从文件中重新读取数据集
     # ds_copy = dcmread(ds.SOPInstanceUID)
-    
+
     # 调用 storeScu
     # storeScu(ds_copy, "192.168.3.222", 6000)
-    store_status_queue.put(ds)
+    move_queue.put(ds)
     # 删除临时文件
     # os.remove(temp_filename)
     # Return a 'Success' status
