3cc6871370439758f2ecf71cfae213b5bc8a1195
Mario <6395323+mario-huang@users.noreply.github.com>
2024-12-02 02:41:07 +0800

    remove handle_get

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from pynetdicom import debug_logger, evt
 from echo_scp import handle_echo
 from find_scp import handle_find
-from get_scp import handle_get
+# from get_scp import handle_get
 from move_scp import handle_move
 from share import ae_scp, config
 from store_scp import handle_store
@@ -12,9 +12,9 @@ if config.debug:
 handlers = [
     (evt.EVT_C_ECHO, handle_echo),
     (evt.EVT_C_FIND, handle_find),
-    (evt.EVT_C_GET, handle_get),
+    # (evt.EVT_C_GET, handle_get),
     (evt.EVT_C_MOVE, handle_move),
-    # (evt.EVT_C_STORE, handle_store),
+    (evt.EVT_C_STORE, handle_store),
 ]
 
 # Start listening for incoming association requests
